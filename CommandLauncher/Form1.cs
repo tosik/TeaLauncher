9a1bdb892512d8aa85260c106bf9a06a0b22b48e
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-01-05 09:42:42 +0900

    fixed for google-ime

--- CommandLauncher/Form1.cs
+++ CommandLauncher/Form1.cs
@@ -172,6 +172,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.";
 
             IMEController ime = new IMEController();
             ime.Off();
+            ime.On();
+            ime.Off();
         }
 
         private void HideWindow()
